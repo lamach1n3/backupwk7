63d0ca97bdc1fef9c26c961b268a12a58b510ae1
adriengobeil3 <adirengobeil3@gmail.com>
2021-02-16 10:22:23 -0500

    added admin and employee booleans to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_135247) do
+ActiveRecord::Schema.define(version: 2021_02_16_151222) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,9 +21,7 @@ ActiveRecord::Schema.define(version: 2021_02_16_135247) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
-    t.boolean "is_admin"
-    t.boolean "is_employee"
-    t.boolean "is_user"
+    t.boolean "employee", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
