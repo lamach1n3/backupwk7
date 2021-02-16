f9d1f8631e9e21971355de6afafe034075782617
eric <eric2.0turcotte@gmail.com>
2021-02-16 15:14:44 -0500

    fix this

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "index#index"
   
   get '/commercial'    => 'pages#commercial'
   get "/home"         => 'pages#index'
