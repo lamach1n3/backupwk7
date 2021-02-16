3349d991b5c8ba5a9e7a8ed9a67de3ecbf641489
adriengobeil3 <adirengobeil3@gmail.com>
2021-02-16 10:58:16 -0500

    added dbs and user status on index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,4 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
-  
-  get '/commercial'    => 'pages#commercial'
-  get "/home"         => 'pages#index'
-  get '/error'        => 'pages#error'
-  get '/quote'        =>  'pages#quote'
-  get '/residential'  => 'pages#residential'
-  # get '/charts'       => 'pages#charts'
-  # get '/diagram'      => 'pages#diagram'
-
-  # post '/leads'       => 'leads#create'
-  # post "/quotes"      => "quotes#create"
-   
-  devise_scope :user do 
-    get "/login" => "devise/sessions#new" 
 end
