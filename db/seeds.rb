edd56fdba866d899db755b8533dd841200fdbb5f
lamach1n3 <taillefer.jf@gmail.com>
2021-02-17 15:39:56 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,30 +12,30 @@ Employee.create(first_name:"Mathieu", last_name:"Lortie", function:"Engineer", p
 Employee.create(first_name:"Thomas", last_name:"Carrier", function:"Engineer", phone:"roc-kets", email:"thomas.carriert@codeboxx.biz" )
 
 
-def employee
-    nicolas = ['nicolas.genest@codeboxx.biz','nicolas','Genest','Nicolas','CEO']
-    nadya = ['nadya.fortier@codeboxx.biz','nadya1','Fortier','Nadya','Director']
-    martin = ['martin.chantal@codeboxx.biz','martin','Chantal','Martin','Director Assistant']
-    mathieuH = ['mathieu.houde@codeboxx.biz','mathieu','Houde','Mathieu','Captain']
-    david = ['david.boutin@codeboxx.biz','david1','Boutin','David','Engineer']
-    mathieuL = ['mathieu.lortie@codeboxx.biz','mathieu','Lortie','Mathieu','Engineer']
-    thomas = ['thomas.carrier@codeboxx.biz','thomas','Carrier','Thomas','Engineer']
-    
-    list_ = [nicolas, nadya, martin, mathieuH, david, mathieuL, thomas]
-    
-    for user in list_ do
-        user_ = User.new
-        user_.email = user[0]
-        user_.password = user[1]
-        user_.add_role :employee
-        employee = Employee.new
-        employee.last_name = user[2]
-        employee.first_name = user[3]
-        employee.function = user[4]
-        user_.employee = employee   
-        user_.save!
-    end
-end
+# def employee
+    # nicolas = ['nicolas.genest@codeboxx.biz','nicolas','Genest','Nicolas','CEO']
+    # nadya = ['nadya.fortier@codeboxx.biz','nadya1','Fortier','Nadya','Director']
+    # martin = ['martin.chantal@codeboxx.biz','martin','Chantal','Martin','Director Assistant']
+    # mathieuH = ['mathieu.houde@codeboxx.biz','mathieu','Houde','Mathieu','Captain']
+    # david = ['david.boutin@codeboxx.biz','david1','Boutin','David','Engineer']
+    # mathieuL = ['mathieu.lortie@codeboxx.biz','mathieu','Lortie','Mathieu','Engineer']
+    # thomas = ['thomas.carrier@codeboxx.biz','thomas','Carrier','Thomas','Engineer']
+    # 
+    # list_ = [nicolas, nadya, martin, mathieuH, david, mathieuL, thomas]
+    # 
+    # for user in list_ do
+        # user_ = User.new
+        # user_.email = user[0]
+        # user_.password = user[1]
+        # user_.add_role :employee
+        # employee = Employee.new
+        # employee.last_name = user[2]
+        # employee.first_name = user[3]
+        # employee.function = user[4]
+        # user_.employee = employee   
+        # user_.save!
+    # end
+# end
 
 
 # how to generete random info from array
@@ -57,26 +57,26 @@ end
 # puts(pl.sample)
 # puts(pl.sample)
 
-# 100.times do
-#     # dateCreationUpdate = Faker::Date.between(from: '2017-09-23', to: '2020-09-25')
-# Quote.create(
-#             install_fees: Faker::Number.between(from: 500, to: 2000),
-#             total_price: Faker::Number.between(from: 50000, to: 200000),
-#             # product_line: Faker::Number.between(from: 1, to: 3),
-#             number_of_apartments: Faker::Number.between(from: 50, to: 200),
-#             number_of_floors: Faker::Number.between(from: 10, to: 70),
-#             number_of_basements: Faker::Number.between(from: 1, to: 10),
-#             number_of_companies: Faker::Number.between(from: 1, to: 5),
-#             number_of_parking: Faker::Number.between(from: 50, to: 200),
-#             maximum_occupancy: Faker::Number.between(from: 50, to: 200),
-#             business_hours: Faker::Number.between(from: 0, to: 24),
-#             number_of_elevators: Faker::Number.between(from: 1, to: 15),
-#             unit_price: Faker::Number.between(from: 11000, to: 15000),
-#             # priceElevator: Faker::Number.between(from: 100000, to: 200000),
-#             # updated_at: dateCreationUpdate,
-#             # created_at:dateCreationUpdate
-#     )
-# end
+100.times do
+    # dateCreationUpdate = Faker::Date.between(from: '2017-09-23', to: '2020-09-25')
+Quote.create(
+            install_fees: Faker::Number.between(from: 500, to: 2000),
+            total_price: Faker::Number.between(from: 50000, to: 200000),
+            # product_line: Faker::Number.between(from: 1, to: 3),
+            number_of_apartments: Faker::Number.between(from: 50, to: 200),
+            number_of_floors: Faker::Number.between(from: 10, to: 70),
+            number_of_basements: Faker::Number.between(from: 1, to: 10),
+            number_of_companies: Faker::Number.between(from: 1, to: 5),
+            number_of_parking: Faker::Number.between(from: 50, to: 200),
+            maximum_occupancy: Faker::Number.between(from: 50, to: 200),
+            business_hours: Faker::Number.between(from: 0, to: 24),
+            number_of_elevators: Faker::Number.between(from: 1, to: 15),
+            unit_price: Faker::Number.between(from: 11000, to: 15000),
+            # priceElevator: Faker::Number.between(from: 100000, to: 200000),
+            # updated_at: dateCreationUpdate,
+            # created_at:dateCreationUpdate
+    )
+end
 # Quote.create(install_fees:23, total_price:3434, number_of_apartments:3424, number_of_floors:2312, number_of_basements:67, number_of_companies:32423)
 
 
