860b3f113944a7953f28f43de04190e74284baf8
officialmxm <dubb3rs1ck@icloud.com>
2021-02-24 15:49:29 -0500

    Revert "Delete leads_controller.rb"
    This reverts commit 6e8cbc2cfdc379233c2ff198704c758c764874d3.

---
+++ app/controllers/leads_controller.rb
@@ -0,0 +1,28 @@
+class LeadsController < ApplicationController
+    def new
+        @lead = Lead.new
+    end
+
+    def create
+        
+        # file = lead_params.delete(:file)
+        # filedata = file.read
+        
+        @lead = Lead.new(lead_params)
+        # @lead.contact_attachment = filedata
+        @lead.save!
+        
+        if @lead.save
+            redirect_to main_app.root_path, notice: "Message sent!"
+        else    
+            redirect_to "/leads", notice: "Invalid fields!"
+        end
+    end
+
+    private
+
+    def lead_params
+      params.require(:lead).permit(:full_name, :email, :phone, :company_name, :project_name, :department, :project_description,
+      :message, :contact_attachment, :file)
+    end
+end
