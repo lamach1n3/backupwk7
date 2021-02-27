2f3c960ac19d2853e863005753b4ac7df2849afa
officialmxm <dubb3rs1ck@icloud.com>
2021-02-26 20:04:30 -0500

    refresh dwh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
+require 'pg'
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
     protect_from_forgery with: :exception
-end
+    
+    
+
+    
+end 
+
