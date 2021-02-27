d68bb35fde31a8a2bf22666b1eaa87560e4f3c8e
officialmxm <dubb3rs1ck@icloud.com>
2021-02-26 21:22:48 -0500

    testin 123

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -16,6 +16,7 @@ class LeadsController < ApplicationController
         
         @lead.save!
         if @lead.save
+            fact_contacts()
             redirect_to main_app.root_path, notice: "Message sent!"
         else    
             redirect_to "/leads", notice: "Invalid fields!"
@@ -23,7 +24,15 @@ class LeadsController < ApplicationController
     end
 
     private
+    def fact_contacts
+      dwh = PG::Connection.new(host: 'codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com', port: 5432, dbname: "MaximeAuger_psql", user: "codeboxx", password: "Codeboxx1!")
+      dwh.exec("TRUNCATE fact_contacts")
 
+      dwh.prepare('to_fact_contacts', 'INSERT INTO fact_contacts (contact_id, creation_date, company_name, email, project_name, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)')
+      Lead.all.each do |ldcontact|
+      dwh.exec_prepared('to_fact_contacts', [ldcontact.id, ldcontact.created_at, ldcontact.company_name, ldcontact.email, ldcontact.project_name])
+      end
+    end
     def lead_params
       params.require(:lead).permit(:full_name, :email, :phone, :company_name, :project_name, :department, :project_description,
       :message, :file_attachment, :file, :image)
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,6 +7,7 @@ class QuotesController < ApplicationController
         @quote = Quote.new(quote_params)
 
         if @quote.final_price != '0.00 $' && @quote.final_price != '-$NaN' && @quote.save
+            fact_quotes()
             redirect_to main_app.root_path, notice: "Quote sent!"
         else    
             redirect_to "/quotes", notice: "Invalid fields!"
@@ -14,7 +15,16 @@ class QuotesController < ApplicationController
     end
 
     private
+    def fact_quotes
+    dwh = PG::Connection.new(host: 'codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com', port: 5432, dbname: "MaximeAuger_psql", user: "codeboxx", password: "Codeboxx1!")
+      dwh.exec("TRUNCATE fact_quotes")
 
+      dwh.prepare('to_fact_quotes', 'INSERT INTO fact_quotes (quote_id, creation, company_name, email, nb_elevator, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)')
+      Quote.all.each do |quotes|
+      dwh.exec_prepared('to_fact_quotes', [quotes.id, quotes.created_at, quotes.quotes_company_name, quotes.quotes_email, quotes.elevator_amount])
+      end
+        
+    end
     def quote_params
       params.require(:quote).permit(:number_of_apartments, :number_of_floors, :company_name, :number_of_basements,
       :number_of_companies, :number_of_corporations, :number_of_parking, :number_of_elevators, :maximum_occupancy,
